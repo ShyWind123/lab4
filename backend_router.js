a8c1a54e43c514c49c7db6caea480f04e0d5bda1
ShyWind <2513561399@qq.com>
2023-12-26 11:44:39 +0800

    12/26/11:44

--- backend_router.js
+++ backend_router.js
@@ -9,18 +9,38 @@ router.use(express.json())
 //查询所有专家
 router.get('/expert/findall', async (req, res) => {
   const experts = await Expert.find();
-  res.send(experts);
+  let experts_res = []
+  for (let i in experts) {
+    experts_res.push({
+      "ID": experts[i].ID,
+      "Name": experts[i].Name,
+      "Score": experts[i].Score,
+      "Introduce": experts[i].Introduce
+    })
+  }
+  console.log(experts_res);
+  res.send(experts_res);
 })
 
 //查询在线专家
 router.get('/expert/findall-online', async (req, res) => {
   const experts = await Expert.find({ Online: true });
-  res.send(experts);
+  let experts_res = []
+  for (let i in experts) {
+    experts_res.push({
+      "ID": experts[i].ID,
+      "Name": experts[i].Name,
+      "Score": experts[i].Score,
+      "Introduce": experts[i].Introduce
+    })
+  }
+  console.log(experts_res);
+  res.send(experts_res);
 })
 
 //通过id查询专家
 router.get('/expert/find/:id', async (req, res) => {
-  const experts = await Expert.find({ ID: req.params.id });
+  let experts = await Expert.find({ ID: req.params.id });
   res.send(experts);
 })
 
